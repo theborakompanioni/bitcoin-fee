1547b20f021f3f8cd0e55ce84067f0614217b255
Bortlesboat <bortstheboat@gmail.com>
2026-04-30 13:48:15 -0400

    Close Satoshi API HTTP client on destroy

--- bitcoin-fee/bitcoin-fee-estimate-satoshiapi/src/test/java/org/tbk/bitcoin/tool/fee/satoshiapi/SatoshiApiFeeApiClientImplTest.java
+++ bitcoin-fee/bitcoin-fee-estimate-satoshiapi/src/test/java/org/tbk/bitcoin/tool/fee/satoshiapi/SatoshiApiFeeApiClientImplTest.java
@@ -13,6 +13,7 @@ import java.nio.charset.StandardCharsets;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SatoshiApiFeeApiClientImplTest {
     private HttpServer server;
@@ -60,5 +61,34 @@ class SatoshiApiFeeApiClientImplTest {
         assertThat(response.getData().getEstimatesOrThrow("1"), is(closeTo(12.5d, 0.0001d)));
         assertThat(response.getData().getEstimatesOrThrow("3"), is(closeTo(7.25d, 0.0001d)));
         assertThat(response.getData().getEstimatesOrThrow("6"), is(closeTo(4.0d, 0.0001d)));
+    }
+
+    @Test
+    void itShouldCloseHttpClientOnDestroy() throws IOException {
+        this.server = HttpServer.create(new InetSocketAddress(0), 0);
+        this.server.createContext("/api/v1/fees/recommended", exchange -> {
+            byte[] response = """
+                    {
+                      "data": {
+                        "recommendation": "Fees are low.",
+                        "estimates": {
+                          "1": 12.5
+                        }
+                      }
+                    }
+                    """.getBytes(StandardCharsets.UTF_8);
+            exchange.sendResponseHeaders(200, response.length);
+            try (OutputStream body = exchange.getResponseBody()) {
+                body.write(response);
+            }
+        });
+        this.server.start();
+
+        String baseUrl = "http://localhost:" + this.server.getAddress().getPort();
+
+        SatoshiApiFeeApiClientImpl client = new SatoshiApiFeeApiClientImpl(baseUrl, null);
+        client.destroy();
+
+        assertThrows(RuntimeException.class, client::feesRecommended);
     }
 }
